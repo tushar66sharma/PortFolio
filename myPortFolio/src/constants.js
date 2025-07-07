1c51d13ca9a91bf3df3cec9020622f7b6bbc1081
Tushar Sharma <tstushar2002@gmail.com>
2025-07-08 03:03:55 +0530

    Contact Section also Done

--- myPortFolio/src/constants.js
+++ myPortFolio/src/constants.js
@@ -152,7 +152,7 @@ export const SkillsInfo = [
     {
       id: 0,
       img: iiitdmj1logo,
-      school: "IIITDM Jabalpur, Jabalpur",
+      school: "IIITDM Jabalpur",
       date: "August 2022 - Present",
       grade: "8.4 CPI",
       desc: "I completed my Bachelor's degree in Computer Science and Engineering (B.Tech.) from Indian Institute of Information Technology Design and Manufacturing Jabalpur. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at IITDMJ allowed me to work on projects that applied theoretical concepts to real-world problems.",
